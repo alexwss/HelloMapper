d628525efcb86f7f0b2c966c88fdd7612c6d90a7
alex <alexwssilva@gmail.com>
2020-08-09 14:24:42 -0300

    creating the structure

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace HelloMapper
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
